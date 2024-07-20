de39cba7b5eb596e1cc073d2f71e31c67e52f980
AnkurGarg07git config --global user.email Ankurgarg89p@gmail.comgit config --listgit config --global user.name AnkurGarg07 <Ankurgarg89p@gmail.com>
2024-07-20 19:25:08 +0530

    Authentication done

---
+++ Account/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path,include
+from . import views
+
+
+
+urlpatterns = [
+    # path('login/', views.login_view, name='login'),
+    path('register/customer/', views.register_customer, name='register_customer'),
+    path('register/seller/', views.register_seller, name='register_seller'),
+    path('login/customer/', views.login_customer, name='login_customer'),
+    path('login/seller/', views.login_seller, name='login_seller'),
+    path('logout/',views.logout_view,name='logout'),
+]
+
--- ArtGround/views.py
+++ ArtGround/views.py
@@ -1,4 +1,17 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
+
+from Account.decorators import seller_required,customer_required
+
+
+@login_required()
+@customer_required
 def index(request):
     return render(request, 'index.html')
+
+
+@login_required()
+@seller_required
+def sellers(request):
+    return render(request, 'sellerIndex.html')
